aee239da8ec3bf832e3ac679733e5a161e24c012
MartinGermain <46567816+MartinGermain@users.noreply.github.com>
2019-02-23 11:32:52 +0100

    Add files via upload
    Lapp qui run le scr

---
+++ FileAttente/App2.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import SideBar from './src/navigation/SideBar'
+export default class App extends Component {
+  render() {
+    return (
+      <SideBar />
+    );
+  }
+}
+
