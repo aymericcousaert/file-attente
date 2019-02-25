afbbc4eddb26d493974e955de266da72da872b36
solenebutruille <47445746+solenebutruille@users.noreply.github.com>
2019-02-25 10:50:33 +0100

    testSoso

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /**************************************
 Class App :
-
+cc je suis la 
 ***************************************/
 import React from 'react';
 import { View, StyleSheet } from 'react-native'
