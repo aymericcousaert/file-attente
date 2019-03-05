2c80fcad82af3e66deda8408cc2333687fc4b34a
YoanSimco <yoan.simiand--cossin@insa-lyon.fr>
2019-03-05 22:49:28 +0100

    Company interface v1.4 (visuel)

--- src/screens/CompanySettings.js
+++ src/screens/CompanySettings.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, ScrollView, Text, TextInput, TouchableOpacity} from 'react-native'
+import {StyleSheet, View, ScrollView, Text, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView} from 'react-native'
 
 class CompanySettings extends React.Component {
 
@@ -9,36 +9,43 @@ class CompanySettings extends React.Component {
 
     render() {
         return (
-            <ScrollView style={styles.main_container} showsVerticalScrollIndicator={false}>
-                <View style={styles.form_container}>
-                    <Text style={styles.text}>Manager's name :</Text>
-                    <TextInput style={styles.text_input} placeholder="SMITH John"/>
-                    <Text style={styles.text}>Name of the company :</Text>
-                    <TextInput style={styles.text_input} placeholder="Starbucks"/>
-                    <Text style={styles.text}>Number and street name :</Text>
-                    <TextInput style={styles.text_input} placeholder="20 Avenue Albert Einstein"/>
-                    <Text style={styles.text}>Postal code :</Text>
-                    <TextInput style={styles.text_input} placeholder="69100"/>
-                    <Text style={styles.text}>City :</Text>
-                    <TextInput style={styles.text_input} placeholder="Villeurbanne"/>
-                    <Text style={styles.text}>Country :</Text>
-                    <TextInput style={styles.text_input} placeholder="FRANCE"/>
-                </View>
-                <View style={styles.touchableOpacity_container}>
-                    <TouchableOpacity style={styles.touchableOpacity} onPress={() => this._displayHomeCompany()}>
-                        <Text style={styles.touchableOpacity_text}>Ok</Text>
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
+            <ImageBackground style={styles.background_container} source={require('../image/BackImageCompany.png')}>
+                <KeyboardAvoidingView style={styles.keyboard_container} behavior="padding">
+                    <ScrollView style={styles.main_container} showsVerticalScrollIndicator={false}>
+                        <Text style={styles.text}>Manager's name :</Text>
+                        <TextInput style={styles.text_input} placeholder="SMITH John"/>
+                        <Text style={styles.text}>Name of the company :</Text>
+                        <TextInput style={styles.text_input} placeholder="Starbucks"/>
+                        <Text style={styles.text}>Number and street name :</Text>
+                        <TextInput style={styles.text_input} placeholder="20 Avenue Albert Einstein"/>
+                        <Text style={styles.text}>Postal code :</Text>
+                        <TextInput style={styles.text_input} placeholder="69100"/>
+                        <Text style={styles.text}>City :</Text>
+                        <TextInput style={styles.text_input} placeholder="Villeurbanne"/>
+                        <Text style={styles.text}>Country :</Text>
+                        <TextInput style={styles.text_input} placeholder="FRANCE"/>
+                        <View style={styles.touchableOpacity_container}>
+                            <TouchableOpacity style={styles.touchableOpacity} onPress={() => this._displayHomeCompany()}>
+                                <Text style={styles.touchableOpacity_text}>Confirm</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </ImageBackground>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    background_container: {
+        flex:1,
+        width:null,
+        height:null,
+    },
     main_container: {
         flex:1
     },
-    form_container: {
+    keyboard_container: {
         flex:1
     },
     touchableOpacity_container: {
@@ -49,20 +56,24 @@ const styles = StyleSheet.create({
         marginTop:10,
         paddingLeft:10,
         fontSize:18,
-        fontWeight:'bold'
+        fontWeight:'bold',
+        color:'white'
     },
     text_input: {
         marginTop:10,
-        marginHorizontal:10,
+        marginHorizontal:20,
+        height:40,
+        borderRadius:25,
         paddingLeft:10,
-        borderWidth:2
+        backgroundColor:'rgba(0, 0, 0, 0.35)',
+        color:'rgba(255, 255, 255, 0.7)'
     },
     touchableOpacity: {
         marginVertical:40,
         justifyContent:'center',
         alignItems:'center',
         height:50,
-        width:100,
+        width:150,
         backgroundColor:'#19D127'
     },
     touchableOpacity_text: {
--- src/screens/HomeCompany.js
+++ src/screens/HomeCompany.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, TouchableOpacity, Image, FlatList, Text} from 'react-native'
+import {StyleSheet, View, TouchableOpacity, Image, ImageBackground, FlatList, Text} from 'react-native'
 import Queue from '../components/Queue'
 
 class HomeCompany extends React.Component {
@@ -18,8 +18,7 @@ class HomeCompany extends React.Component {
 
     render() {
         return (
-            <View style={styles.main_container}>
-                <Image style={styles.back_image} source={require('../image/BackImageCompany.png')}/>
+            <ImageBackground style={styles.background_container} source={require('../image/BackImageCompany.png')}>
                 <TouchableOpacity style={styles.settings_touchableOpacity} onPress={() => this._displayCompanySettings()}>
                     <Image style={styles.settings_image} source={require('../icon/settings.png')}/>
                 </TouchableOpacity>
@@ -39,26 +38,21 @@ class HomeCompany extends React.Component {
                 <TouchableOpacity style={styles.add_queue_touchableOpacity} onPress={() => this._displayQueueSettings()}>
                     <Text style={styles.add_queue_text}>+</Text>
                 </TouchableOpacity>
-            </View>
+            </ImageBackground>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex:1
+    background_container: {
+        flex:1,
+        width:null,
+        height:null,
     },
     flatlist_container: {
         flex:1,
         justifyContent:'center',
         alignItems:'center'
-    },
-    back_image: {
-        flex:1,
-        position:'absolute',
-        resizeMode:'cover',
-        height:'100%',
-        width:'100%'
     },
     settings_touchableOpacity: {
         height:40,
