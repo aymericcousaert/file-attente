2abdab92a259c84060761a32fc9d3a47273e0b51
YoanSimco <yoan.simiand--cossin@insa-lyon.fr>
2019-03-20 21:41:46 +0100

    Merging branchs

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 /**************************************
 Class App :
-cc je suis la
+
 ***************************************/
 import React from 'react';
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, StatusBar } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
-import AppNavigator from './src/navigation/AppNavigator';
-import ApiKeys from './src/config/firebase/ApiKeys'
+import LoggedOutStack from './src/navigation/AuthStackNavigator';
+import ApiKeys from './src/config/firebase/ApiKeys';
+import config from './src/config/';
 import * as firebase from 'firebase';
 import SideBar from './src/navigation/SideBar';
-import HomeCompany from './src/screens/HomeCompany'
-import CompanySettings from './src/screens/CompanySettings'
-import QueueDetails from './src/screens/QueueDetails'
-import QueueSettings from './src/screens/QueueSettings'
+import Mainscreen from './src/screens/MainScreen';
 
 /**************************************
 Creation of the App containers one that
 routes to the home page (AppHome) and one
-that routes to the app's login (AppScreens)
+that routes to the app's login (AuthScreens)
 ***************************************/
-const AppScreens = createAppContainer(AppNavigator);
+const AuthScreens = createAppContainer(LoggedOutStack);
 const HomeNavigator = createStackNavigator(
 	{
 		Home: SideBar,
@@ -33,24 +31,6 @@ const HomeNavigator = createStackNavigator(
 )
 const AppHome = createAppContainer(HomeNavigator);
 
-/*NAVIGATION OF COMPANY*/
-const CompanyStackNavigator = createStackNavigator({
-	HomeCompany: {
-		screen:HomeCompany
-	},
-	CompanySettings: {
-		screen:CompanySettings
-	},
-	QueueDetails: {
-		screen:QueueDetails
-	},
-	QueueSettings: {
-		screen:QueueSettings
-	}
-})
-const AppCompany = createAppContainer(CompanyStackNavigator)
-/***************************************/
-
 export default class App extends React.Component {
 
 	constructor(props) {
@@ -63,14 +43,22 @@ export default class App extends React.Component {
 		Initializing firebase and checks if user
 		is Authenticated, if not we redirect him
 		to the AppScreens else he goes directly
-		to the HomeScreen (thanks to isAuthenticated).
+		to the Home (thanks to isAuthenticated).
 		***************************************/
 		if (!firebase.apps.length) { firebase.initializeApp(ApiKeys.FirebaseConfig); }
 		firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
 	}
+
 	onAuthStateChanged = (user) => {
-		this.setState({ isAuthenticationReady: true });
-		this.setState({ isAuthenticated: !!user });
+		var userEmail = "";
+		if (user) { userEmail = user.email }
+		firebase.database().ref('users/').orderByChild("email").equalTo(userEmail).once("value").then(snapshot => {
+			if (snapshot.val()) {
+				config.userDetails.uid = Object.keys(snapshot.val())[0];
+				this.setState({ isAuthenticationReady: true });
+				this.setState({ isAuthenticated: !!user });
+			};
+		});
 	}
 
 	render() {
@@ -82,7 +70,10 @@ export default class App extends React.Component {
 			Checks if the user can go directly to
 			the App's home or not
 			***************************************/
-			<AppCompany/> /*J'ai modifié cette ligne pour se concentrer sur la partie entreprise*/
+			<View style={styles.container}>
+				<StatusBar barStyle='light-content' />
+				{this.state.isAuthenticated ? <AppHome /> : <AuthScreens />}
+			</View>
 		);
 	}
 }
